20863e719120927d0b9d1ae6e8ce1947842387e2
bar molaga <bar@molaga.com>
2014-10-29 06:57:39 +0200

    o

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngSanitize']);
+/*var app = angular.module('app', ['ngSanitize']);
 app.controller('ctrler', function ($scope) {
 	$scope.customers = [
 	{ name: 'aaaaaa', last: '<h1 class="nicy" style="font-size:9110px;">aaaaaa</h1>' },
@@ -7,4 +7,29 @@ app.controller('ctrler', function ($scope) {
 	{ name: 'qwef', last: 'qwf' }
 	];
 	$scope.myHtml = '<h1 style="color:#0C3;">aaaaaaa</h1>';
+	$scope.komo = function () {
+		return komo.html('sdcasdc');
+	}
+});*/
+var app = angular.module('app', []);
+
+app.directive('dynamic', function ($compile) {
+  return {
+    restrict: 'A',
+    replace: true,
+    link: function (scope, ele, attrs) {
+    	console.log(attrs);
+      scope.$watch(attrs.dynamic, function(html) {
+        ele.html(html);
+        $compile(ele.contents())(scope);
+      });
+    }
+  };
 });
+
+function MyController($scope) {
+  $scope.click = function(arg) {
+    alert('Clicked ' + arg);
+  }
+  $scope.html = '<a ng-click="click(1)" href="#">Click me</a>';
+}
